461372f028b3eb160ae189b113e5a94c08632746
wong <degawong@hotmail.com>
2020-10-15 20:25:29 +0800

    [0005]

--- 0005/0005.hpp
+++ 0005/0005.hpp
@@ -1,11 +1,12 @@
 /*
  * @Author: your name
- * @Date: 2020-09-30 14:33:38
- * @LastEditTime: 2020-09-30 14:37:46
+ * @Date: 2020-10-14 16:04:25
+ * @LastEditTime: 2020-10-15 20:24:21
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
- * @FilePath: \leetcode\003.cpp
+ * @FilePath: \leetcode\0005\0005.hpp
  */
+
 #include <cmath>
 #include <string>
 #include <iostream>
@@ -16,21 +17,22 @@ using namespace std;
 
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-		int temp = 0;
-		int result = 0;
-		int size = s.size();
-		unordered_set<char> sub;
-		int next = -1;
-		for (int i = 0; i < size; ++i) {
-			if (0 != i) {
-				sub.erase(s[i - 1]);
-			}
-			while ((next < size - 1) && (!sub.count(s[next + 1]))) {
-				sub.insert(s[(next++) + 1]);
-			}
-			result = max(next - i + 1, result);
-		}
-		return result;
+    string longestPalindrome(string s) {
+        string ret;
+        for(int i = 0; i < s.size(); ++i) {
+            auto ls = Palindrome(s, i, i);
+            auto rs = Palindrome(s, i, i + 1);
+            ret = ret.size() > ls.size() ? ret : ls;
+            ret = ret.size() > rs.size() ? ret : rs;
+        }
+        return ret;
     }
+private:
+	string Palindrome(string s, int l, int r) {
+		while(l >= 0 && r < s.size() && s[l] == s[r]) {
+            --l;
+            ++r;
+        }
+        return s.substr(l + 1, r - l - 1);
+	}
 };
