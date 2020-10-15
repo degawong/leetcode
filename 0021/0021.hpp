e81931a56c638c7185a04406ed68637beb54749e
wong <degawong@hotmail.com>
2020-10-15 09:54:14 +0800

    [0021]

--- 0021/0021.hpp
+++ 0021/0021.hpp
@@ -1,11 +1,12 @@
 /*
  * @Author: your name
- * @Date: 2020-09-30 14:33:38
- * @LastEditTime: 2020-09-30 14:37:46
+ * @Date: 2020-10-14 16:04:25
+ * @LastEditTime: 2020-10-15 09:54:01
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
- * @FilePath: \leetcode\003.cpp
+ * @FilePath: \leetcode\0021\0021.hpp
  */
+
 #include <cmath>
 #include <string>
 #include <iostream>
@@ -14,23 +15,31 @@
 
 using namespace std;
 
+// leetcode : https://leetcode-cn.com/problems/merge-two-sorted-lists/
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode() : val(0), next(nullptr) {}
+	ListNode(int x) : val(x), next(nullptr) {}
+	ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-		int temp = 0;
-		int result = 0;
-		int size = s.size();
-		unordered_set<char> sub;
-		int next = -1;
-		for (int i = 0; i < size; ++i) {
-			if (0 != i) {
-				sub.erase(s[i - 1]);
-			}
-			while ((next < size - 1) && (!sub.count(s[next + 1]))) {
-				sub.insert(s[(next++) + 1]);
-			}
-			result = max(next - i + 1, result);
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+		ListNode* ret = nullptr;
+		if(l1 == nullptr) return l2;
+		if(l2 == nullptr) return l1;
+        if (l1->val <= l2->val) {
+			ret = l1;
+			ret->next = mergeTwoLists(l1->next, l2);
+		} else {
+			ret = l2;
+			ret->next = mergeTwoLists(l1, l2->next);
 		}
-		return result;
+		return ret;
     }
 };
+// reference
+// https://blog.csdn.net/xiangxianghehe/article/details/81739181
