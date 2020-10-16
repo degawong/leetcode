63f9c391c4bdfbf4fe98c79a116d138561e055a1
wong <degawong@hotmail.com>
2020-10-16 15:17:49 +0800

    [0142]

--- 0142/0142.hpp
+++ 0142/0142.hpp
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-09-30 14:33:38
- * @LastEditTime: 2020-09-30 14:37:46
+ * @LastEditTime: 2020-10-16 15:17:02
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \leetcode\003.cpp
@@ -14,23 +14,29 @@
 
 using namespace std;
 
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(nullptr) {}
+};
+
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-		int temp = 0;
-		int result = 0;
-		int size = s.size();
-		unordered_set<char> sub;
-		int next = -1;
-		for (int i = 0; i < size; ++i) {
-			if (0 != i) {
-				sub.erase(s[i - 1]);
+    ListNode *detectCycle(ListNode *head) {
+		auto fast = head;
+		auto slow = head;
+		while(fast != nullptr && fast->next != nullptr) {
+			slow = slow->next;
+			fast = fast->next->next;
+			if(fast == slow) {
+				slow = head;
+				while(slow != fast) {
+					fast = fast->next;
+					slow = slow->next;					
+				}
+				return slow;
 			}
-			while ((next < size - 1) && (!sub.count(s[next + 1]))) {
-				sub.insert(s[(next++) + 1]);
-			}
-			result = max(next - i + 1, result);
 		}
-		return result;
+		return nullptr;
     }
 };
