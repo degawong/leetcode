06d93343cc7a6657b8d73e041170e8189a1d0ab9
wong <degawong@hotmail.com>
2020-10-21 14:19:18 +0800

    [0257]

--- 0257/0257.hpp
+++ 0257/0257.hpp
@@ -1,36 +1,48 @@
 /*
  * @Author: your name
- * @Date: 2020-09-30 14:33:38
- * @LastEditTime: 2020-09-30 14:37:46
+ * @Date: 2020-10-14 16:04:27
+ * @LastEditTime: 2020-10-21 14:17:54
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
- * @FilePath: \leetcode\003.cpp
+ * @FilePath: \leetcode\0257\0257.hpp
  */
+
 #include <cmath>
 #include <string>
+#include <vector>
 #include <iostream>
 #include <algorithm>
 #include <unordered_set>
 
 using namespace std;
 
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-		int temp = 0;
-		int result = 0;
-		int size = s.size();
-		unordered_set<char> sub;
-		int next = -1;
-		for (int i = 0; i < size; ++i) {
-			if (0 != i) {
-				sub.erase(s[i - 1]);
-			}
-			while ((next < size - 1) && (!sub.count(s[next + 1]))) {
-				sub.insert(s[(next++) + 1]);
-			}
-			result = max(next - i + 1, result);
-		}
-		return result;
+    vector<string> binaryTreePaths(TreeNode* root) {
+        vector<string> paths;
+        construct_paths(root, "", paths);
+        return paths;
+    }
+private:
+    void construct_paths(TreeNode* root, string path, vector<string>& paths) {
+        if (root != nullptr) {
+            path += to_string(root->val);
+            if (root->left == nullptr && root->right == nullptr) {
+                paths.push_back(path);
+            } else {
+                path += "->";
+                construct_paths(root->left, path, paths);
+                construct_paths(root->right, path, paths);
+            }
+        }
     }
 };
+
+
