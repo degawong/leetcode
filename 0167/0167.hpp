bd908b32d96194825d6ee193be664fe5e96d7989
wong <degawong@hotmail.com>
2020-10-21 18:04:56 +0800

    [0167]

--- 0167/0167.hpp
+++ 0167/0167.hpp
@@ -1,11 +1,12 @@
 /*
  * @Author: your name
- * @Date: 2020-09-30 14:33:38
- * @LastEditTime: 2020-09-30 14:37:46
+ * @Date: 2020-10-14 16:04:26
+ * @LastEditTime: 2020-10-21 18:04:05
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
- * @FilePath: \leetcode\003.cpp
+ * @FilePath: \leetcode\0167\0167.hpp
  */
+
 #include <cmath>
 #include <string>
 #include <iostream>
@@ -16,21 +17,18 @@ using namespace std;
 
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-		int temp = 0;
-		int result = 0;
-		int size = s.size();
-		unordered_set<char> sub;
-		int next = -1;
-		for (int i = 0; i < size; ++i) {
-			if (0 != i) {
-				sub.erase(s[i - 1]);
+    vector<int> twoSum(vector<int>& numbers, int target) {
+		int slow = 0;
+		int fast = numbers.size() - 1;
+		while(slow < fast) {
+			if(numbers[slow] + numbers[fast] == target) {
+				return { slow + 1, fast + 1 };
+			} else if(numbers[slow] + numbers[fast] < target) {
+				++slow;
+			} else {
+				--fast;
 			}
-			while ((next < size - 1) && (!sub.count(s[next + 1]))) {
-				sub.insert(s[(next++) + 1]);
-			}
-			result = max(next - i + 1, result);
 		}
-		return result;
+		return { -1, -1 };
     }
 };
