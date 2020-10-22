1fb09490757bb1ce1b4a1096ca736aede16267f7
wong <degawong@hotmail.com>
2020-10-22 09:37:18 +0800

    [0053]

--- 0053/0053.hpp
+++ 0053/0053.hpp
@@ -1,11 +1,12 @@
 /*
  * @Author: your name
- * @Date: 2020-09-30 14:33:38
- * @LastEditTime: 2020-09-30 14:37:46
+ * @Date: 2020-10-14 16:04:26
+ * @LastEditTime: 2020-10-22 09:36:48
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
- * @FilePath: \leetcode\003.cpp
+ * @FilePath: \leetcode\0053\0053.hpp
  */
+
 #include <cmath>
 #include <string>
 #include <iostream>
@@ -16,21 +17,26 @@ using namespace std;
 
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-		int temp = 0;
-		int result = 0;
-		int size = s.size();
-		unordered_set<char> sub;
-		int next = -1;
-		for (int i = 0; i < size; ++i) {
-			if (0 != i) {
-				sub.erase(s[i - 1]);
-			}
-			while ((next < size - 1) && (!sub.count(s[next + 1]))) {
-				sub.insert(s[(next++) + 1]);
-			}
-			result = max(next - i + 1, result);
+    int maxSubArray(vector<int>& nums) {
+		if(nums.size() == 0) return 0;
+		auto temp = 0;
+		auto ret = nums[0];
+		for(auto n : nums) {
+			temp = max(temp + n, n);
+			ret = max(ret, temp);
 		}
-		return result;
+		return ret;
+		// initial version of memory search
+		// if(nums.size() == 0) return 0;
+		// vector<int> dp(nums.size(), 0);
+		// dp[0] = nums[0];
+		// for(int i = 1; i < nums.size(); ++i) {
+		// 	dp[i] = max(dp[i - 1] + nums[i], nums[i]);
+		// }
+		// auto ret = 0;
+		// for(auto m : dp) {
+		// 	if(ret < m) ret = m;
+		// }
+		// return ret;		
     }
 };
