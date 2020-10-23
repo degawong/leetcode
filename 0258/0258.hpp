73b5db2328b70c1cb7185ce371efac653aa92fba
wong <degawong@hotmail.com>
2020-10-23 15:59:10 +0800

    [0258]

--- 0258/0258.hpp
+++ 0258/0258.hpp
@@ -1,11 +1,12 @@
 /*
  * @Author: your name
- * @Date: 2020-09-30 14:33:38
- * @LastEditTime: 2020-09-30 14:37:46
- * @LastEditors: Please set LastEditors
+ * @Date: 2020-10-14 16:04:27
+ * @LastEditTime: 2020-10-23 15:59:00
+ * @LastEditors: your name
  * @Description: In User Settings Edit
- * @FilePath: \leetcode\003.cpp
+ * @FilePath: \leetcode\0258\0258.hpp
  */
+
 #include <cmath>
 #include <string>
 #include <iostream>
@@ -16,21 +17,13 @@ using namespace std;
 
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-		int temp = 0;
-		int result = 0;
-		int size = s.size();
-		unordered_set<char> sub;
-		int next = -1;
-		for (int i = 0; i < size; ++i) {
-			if (0 != i) {
-				sub.erase(s[i - 1]);
-			}
-			while ((next < size - 1) && (!sub.count(s[next + 1]))) {
-				sub.insert(s[(next++) + 1]);
-			}
-			result = max(next - i + 1, result);
-		}
-		return result;
+    int addDigits(int num) {
+        int ret = 0;
+        if(num < 10) return num;
+        while(num > 0) {
+            ret += num % 10;
+            num = num / 10;
+        }
+        return addDigits(ret);
     }
 };
