587bd6a749630784c69d9904ddb01f1887b09445
wong <degawong@hotmail.com>
2020-10-23 13:59:30 +0800

    [0204]

--- 0204/0204.hpp
+++ 0204/0204.hpp
@@ -1,11 +1,12 @@
 /*
  * @Author: your name
- * @Date: 2020-09-30 14:33:38
- * @LastEditTime: 2020-09-30 14:37:46
+ * @Date: 2020-10-14 16:04:27
+ * @LastEditTime: 2020-10-23 13:58:38
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
- * @FilePath: \leetcode\003.cpp
+ * @FilePath: \leetcode\0204\0204.hpp
  */
+
 #include <cmath>
 #include <string>
 #include <iostream>
@@ -15,22 +16,19 @@
 using namespace std;
 
 class Solution {
+	// Eratosthenes
 public:
-    int lengthOfLongestSubstring(string s) {
-		int temp = 0;
-		int result = 0;
-		int size = s.size();
-		unordered_set<char> sub;
-		int next = -1;
-		for (int i = 0; i < size; ++i) {
-			if (0 != i) {
-				sub.erase(s[i - 1]);
+    int countPrimes(int n) {
+		int ret = 0;
+		vector<bool> signs(n, true);
+		for (int i = 2; i < n; ++i) {
+			if (signs[i]) {
+				ret += 1;
+				for (int j = i + i; j < n; j += i) {
+					signs[j] = false;
+				}
 			}
-			while ((next < size - 1) && (!sub.count(s[next + 1]))) {
-				sub.insert(s[(next++) + 1]);
-			}
-			result = max(next - i + 1, result);
 		}
-		return result;
+    	return ret;
     }
 };
