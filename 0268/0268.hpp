2b30d0f6760c5a1df2621927d99f18f990834d51
wong <degawong@hotmail.com>
2020-10-23 16:28:20 +0800

    [0268]

--- 0268/0268.hpp
+++ 0268/0268.hpp
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-09-30 14:33:38
- * @LastEditTime: 2020-09-30 14:37:46
+ * @LastEditTime: 2020-10-23 16:27:53
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \leetcode\003.cpp
@@ -16,21 +16,12 @@ using namespace std;
 
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-		int temp = 0;
-		int result = 0;
-		int size = s.size();
-		unordered_set<char> sub;
-		int next = -1;
-		for (int i = 0; i < size; ++i) {
-			if (0 != i) {
-				sub.erase(s[i - 1]);
-			}
-			while ((next < size - 1) && (!sub.count(s[next + 1]))) {
-				sub.insert(s[(next++) + 1]);
-			}
-			result = max(next - i + 1, result);
-		}
-		return result;
+    int missingNumber(vector<int>& nums) {
+        auto sum = 0;
+        auto size = nums.size();
+        for(auto n : nums) {
+            sum += n;
+        }
+        return size * (size + 1) / 2 - sum;
     }
 };
