dd01e433a82692bfefc0974b459e80fd790c6787
wong <degawong@hotmail.com>
2020-10-26 16:08:37 +0800

    [0113]

--- 0113/0113.hpp
+++ 0113/0113.hpp
@@ -1,36 +1,57 @@
 /*
  * @Author: your name
- * @Date: 2020-09-30 14:33:38
- * @LastEditTime: 2020-09-30 14:37:46
+ * @Date: 2020-10-14 16:04:26
+ * @LastEditTime: 2020-10-26 16:07:01
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
- * @FilePath: \leetcode\003.cpp
+ * @FilePath: \leetcode\0113\0113.hpp
  */
+
 #include <cmath>
 #include <string>
 #include <iostream>
 #include <algorithm>
+#include <unordered_map>
 #include <unordered_set>
 
 using namespace std;
 
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+ 
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-		int temp = 0;
-		int result = 0;
-		int size = s.size();
-		unordered_set<char> sub;
-		int next = -1;
-		for (int i = 0; i < size; ++i) {
-			if (0 != i) {
-				sub.erase(s[i - 1]);
-			}
-			while ((next < size - 1) && (!sub.count(s[next + 1]))) {
-				sub.insert(s[(next++) + 1]);
-			}
-			result = max(next - i + 1, result);
-		}
-		return result;
+    vector<vector<int>> pathSum(TreeNode* root, int sum) {
+        dfs(root, sum);
+		return ret;
     }
+private:
+    void dfs(TreeNode* root, int sum) {
+        if (root == nullptr) return;
+        path.emplace_back(root->val);
+        if (root->left == nullptr && root->right == nullptr && sum == root->val) {
+            ret.emplace_back(path);
+        }
+        dfs(root->left, sum - root->val);
+        dfs(root->right, sum - root->val);
+        path.pop_back();
+    }
+private:
+    unordered_map<TreeNode*, TreeNode*> parent;
+    void getPath(TreeNode* node) {
+        vector<int> tmp;
+        while (node != nullptr) {
+            tmp.emplace_back(node->val);
+            node = parent[node];
+        }
+        reverse(tmp.begin(), tmp.end());
+        ret.emplace_back(tmp);
+    }
+private:
+    vector<int> path;		
+    vector<vector<int>> ret;
 };
