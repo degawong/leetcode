45b4760c2da2b1126b1ef02ffd7ef9ceea451500
wong <degawong@hotmail.com>
2020-10-27 14:52:29 +0800

    [0337]

--- 0337/0337.hpp
+++ 0337/0337.hpp
@@ -1,36 +1,43 @@
 /*
  * @Author: your name
- * @Date: 2020-09-30 14:33:38
- * @LastEditTime: 2020-09-30 14:37:46
+ * @Date: 2020-10-14 16:04:28
+ * @LastEditTime: 2020-10-27 14:50:15
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
- * @FilePath: \leetcode\003.cpp
+ * @FilePath: \leetcode\0337\0337.hpp
  */
+
 #include <cmath>
 #include <string>
 #include <iostream>
 #include <algorithm>
 #include <unordered_set>
+#include <unordered_map>
 
 using namespace std;
 
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-		int temp = 0;
-		int result = 0;
-		int size = s.size();
-		unordered_set<char> sub;
-		int next = -1;
-		for (int i = 0; i < size; ++i) {
-			if (0 != i) {
-				sub.erase(s[i - 1]);
-			}
-			while ((next < size - 1) && (!sub.count(s[next + 1]))) {
-				sub.insert(s[(next++) + 1]);
-			}
-			result = max(next - i + 1, result);
-		}
-		return result;
+    int rob(TreeNode* root) {
+        dfs(root);
+        return max(f[root], g[root]);
     }
+private:
+	// f current node max value
+	// g current leaf node max value
+    unordered_map<TreeNode*, int> f, g;
+    void dfs(TreeNode* node) {
+		if(node == nullptr) return;
+        dfs(node->left);
+        dfs(node->right);
+        f[node] = node->val + g[node->left] + g[node->right];
+        g[node] = max(f[node->left], g[node->left]) + max(f[node->right], g[node->right]);
+    }	
 };
