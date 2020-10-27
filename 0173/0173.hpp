cca4d75a3ce0727adab69ac94ff053c0df397d26
wong <degawong@hotmail.com>
2020-10-27 10:15:31 +0800

    [0173]

--- 0173/0173.hpp
+++ 0173/0173.hpp
@@ -1,36 +1,54 @@
 /*
  * @Author: your name
- * @Date: 2020-09-30 14:33:38
- * @LastEditTime: 2020-09-30 14:37:46
+ * @Date: 2020-10-14 16:04:26
+ * @LastEditTime: 2020-10-27 10:13:30
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
- * @FilePath: \leetcode\003.cpp
+ * @FilePath: \leetcode\0173\0173.hpp
  */
+
 #include <cmath>
+#include <deque>
+#include <queue>
 #include <string>
+#include <vector>
 #include <iostream>
 #include <algorithm>
 #include <unordered_set>
 
 using namespace std;
 
-class Solution {
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+  
+class BSTIterator {
 public:
-    int lengthOfLongestSubstring(string s) {
-		int temp = 0;
-		int result = 0;
-		int size = s.size();
-		unordered_set<char> sub;
-		int next = -1;
-		for (int i = 0; i < size; ++i) {
-			if (0 != i) {
-				sub.erase(s[i - 1]);
-			}
-			while ((next < size - 1) && (!sub.count(s[next + 1]))) {
-				sub.insert(s[(next++) + 1]);
-			}
-			result = max(next - i + 1, result);
-		}
-		return result;
+    BSTIterator(TreeNode* root) {
+		inorder(root);
     }
+    
+    /** @return the next smallest number */
+    int next() {
+		auto node = q.front();
+		q.pop_front();
+		return node->val;
+    }
+    
+    /** @return whether we have a next smallest number */
+    bool hasNext() {
+		return !q.empty();
+    }
+private:
+	void inorder(TreeNode* node) {
+		if(node == nullptr) return;
+		inorder(node->left);
+		q.push_back(node);
+		inorder(node->right);
+	}
+private:
+	deque<TreeNode*> q;
 };
