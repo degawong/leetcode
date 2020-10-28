898174a56fff8b66cdc65c1ca40444366a70b9ca
wong <degawong@hotmail.com>
2020-10-28 16:36:57 +0800

    [0623]

--- 0623/0623.hpp
+++ 0623/0623.hpp
@@ -1,12 +1,14 @@
 /*
  * @Author: your name
- * @Date: 2020-09-30 14:33:38
- * @LastEditTime: 2020-09-30 14:37:46
+ * @Date: 2020-10-14 16:04:30
+ * @LastEditTime: 2020-10-28 16:36:39
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
- * @FilePath: \leetcode\003.cpp
+ * @FilePath: \leetcode\0623\0623.hpp
  */
+
 #include <cmath>
+#include <deque>
 #include <string>
 #include <iostream>
 #include <algorithm>
@@ -14,23 +16,48 @@
 
 using namespace std;
 
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-		int temp = 0;
-		int result = 0;
-		int size = s.size();
-		unordered_set<char> sub;
-		int next = -1;
-		for (int i = 0; i < size; ++i) {
-			if (0 != i) {
-				sub.erase(s[i - 1]);
+    TreeNode* addOneRow(TreeNode* root, int v, int d) {
+		int level = 0;
+		deque<TreeNode*> dq;
+		dq.push_back(root);
+        if(d == 1) {
+            auto left = root;
+            root = new TreeNode(v);
+            root->left = left;
+            return root;
+        }
+		while(!dq.empty()) {
+			auto size = dq.size();
+			if(++level == d - 1) {
+				for(int i = 0; i < size; ++i) {
+					auto node = dq.front();
+					dq.pop_front();
+					auto left = node->left;
+					auto right = node->right;
+					node->left = new TreeNode(v);
+					node->right = new TreeNode(v);
+					node->left->left = left;
+					node->right->right = right;
+				}
+				return root;
+			} else {
+				for(int i = 0; i < size; ++i) {
+					auto node = dq.front();
+					dq.pop_front();
+					if(node->left != nullptr) dq.push_back(node->left);
+					if(node->right != nullptr) dq.push_back(node->right);
+				}
 			}
-			while ((next < size - 1) && (!sub.count(s[next + 1]))) {
-				sub.insert(s[(next++) + 1]);
-			}
-			result = max(next - i + 1, result);
 		}
-		return result;
+		return root;
     }
 };
