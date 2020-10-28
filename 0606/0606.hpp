eec81c22d004f3624339a8932d7ecf5b211e6a72
wong <degawong@hotmail.com>
2020-10-28 10:50:50 +0800

    [0606]

--- 0606/0606.hpp
+++ 0606/0606.hpp
@@ -1,11 +1,12 @@
 /*
  * @Author: your name
- * @Date: 2020-09-30 14:33:38
- * @LastEditTime: 2020-09-30 14:37:46
+ * @Date: 2020-10-14 16:04:30
+ * @LastEditTime: 2020-10-28 10:49:47
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
- * @FilePath: \leetcode\003.cpp
+ * @FilePath: \leetcode\0606\0606.hpp
  */
+
 #include <cmath>
 #include <string>
 #include <iostream>
@@ -13,24 +14,44 @@
 #include <unordered_set>
 
 using namespace std;
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
 
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-		int temp = 0;
-		int result = 0;
-		int size = s.size();
-		unordered_set<char> sub;
-		int next = -1;
-		for (int i = 0; i < size; ++i) {
-			if (0 != i) {
-				sub.erase(s[i - 1]);
-			}
-			while ((next < size - 1) && (!sub.count(s[next + 1]))) {
-				sub.insert(s[(next++) + 1]);
-			}
-			result = max(next - i + 1, result);
+    string tree2str(TreeNode* t) {
+		string ret;
+		if(t == nullptr) return ret;
+		ret += to_string(t->val);
+		if((t->right != nullptr) && (t->left == nullptr)) {
+			ret += "()";
 		}
-		return result;
+		ret += node2str(t->left);
+		ret += node2str(t->right);
+		return ret;
+    }
+private:
+    string node2str(TreeNode* t) {
+		string ret;
+		if(t == nullptr) return ret;
+		ret.push_back('(');
+		ret += to_string(t->val);
+		if(t->left == nullptr && t->right == nullptr) {			
+		}
+		else if(t->left == nullptr) {
+			ret += "()";
+			ret += node2str(t->right);
+		} else if(t->right == nullptr) {
+			ret += node2str(t->left);
+		} else {
+			ret += node2str(t->left);
+			ret += node2str(t->right);
+		}
+		ret.push_back(')');
+		return ret;
     }
 };
