8efc3e253f36b6532a1e3335f2c3edd97c1b76a2
wong <degawong@hotmail.com>
2020-10-29 15:25:28 +0800

    [0671]

--- 0671/0671.hpp
+++ 0671/0671.hpp
@@ -1,11 +1,13 @@
 /*
  * @Author: your name
- * @Date: 2020-09-30 14:33:38
- * @LastEditTime: 2020-09-30 14:37:46
+ * @Date: 2020-10-14 16:04:30
+ * @LastEditTime: 2020-10-29 15:25:19
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
- * @FilePath: \leetcode\003.cpp
+ * @FilePath: \leetcode\0671\0671.hpp
  */
+
+#include <set>
 #include <cmath>
 #include <string>
 #include <iostream>
@@ -14,23 +16,36 @@
 
 using namespace std;
 
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-		int temp = 0;
-		int result = 0;
-		int size = s.size();
-		unordered_set<char> sub;
-		int next = -1;
-		for (int i = 0; i < size; ++i) {
-			if (0 != i) {
-				sub.erase(s[i - 1]);
+    int findSecondMinimumValue(TreeNode* root) {
+		inorder(root);
+		int ret = -1;
+		int index = 0;
+		if(s.size() == 1) return -1;
+		else {
+			for(auto i = s.begin(); i != s.end(); ++i) {
+				if(index++ == 1) ret = *i;
 			}
-			while ((next < size - 1) && (!sub.count(s[next + 1]))) {
-				sub.insert(s[(next++) + 1]);
-			}
-			result = max(next - i + 1, result);
 		}
-		return result;
+		return ret;
     }
+private:
+	void inorder(TreeNode* node) {
+		if(node == nullptr) return;
+		inorder(node->left);
+		s.insert(node->val);
+		inorder(node->right);
+	}
+private:
+	set<int> s;
 };
