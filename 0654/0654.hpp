45c20e8cead7e7277189b4a9b9d6c94349abec5b
wong <degawong@hotmail.com>
2020-10-29 11:25:08 +0800

    [0654]

--- 0654/0654.hpp
+++ 0654/0654.hpp
@@ -1,11 +1,12 @@
 /*
  * @Author: your name
- * @Date: 2020-09-30 14:33:38
- * @LastEditTime: 2020-09-30 14:37:46
+ * @Date: 2020-10-14 16:04:30
+ * @LastEditTime: 2020-10-29 11:24:00
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
- * @FilePath: \leetcode\003.cpp
+ * @FilePath: \leetcode\0654\0654.hpp
  */
+
 #include <cmath>
 #include <string>
 #include <iostream>
@@ -14,23 +15,30 @@
 
 using namespace std;
 
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+ 
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-		int temp = 0;
-		int result = 0;
-		int size = s.size();
-		unordered_set<char> sub;
-		int next = -1;
-		for (int i = 0; i < size; ++i) {
-			if (0 != i) {
-				sub.erase(s[i - 1]);
+    TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
+		if(nums.size() == 0) return nullptr;
+		int index = 0;
+		int value = INT_MIN;
+		for(int i = 0; i < nums.size(); ++i) {
+			if(nums[i] > value) {
+				index = i;
+				value = nums[i];
 			}
-			while ((next < size - 1) && (!sub.count(s[next + 1]))) {
-				sub.insert(s[(next++) + 1]);
-			}
-			result = max(next - i + 1, result);
 		}
-		return result;
+		auto node = new TreeNode(nums[index]);
+		auto left = vector<int>{ nums.begin(), nums.begin() + index };
+		auto right = vector<int>{ nums.begin() + index + 1, nums.end() };
+		node->left = constructMaximumBinaryTree(left);
+		node->right = constructMaximumBinaryTree(right);
+		return node;
     }
 };
