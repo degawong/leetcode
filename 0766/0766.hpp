77771848b8b434d58b7ecb17a66643f267f4ef95
wong <degawong@hotmail.com>
2020-10-30 16:04:37 +0800

    [0766]

--- 0766/0766.hpp
+++ 0766/0766.hpp
@@ -1,11 +1,12 @@
 /*
  * @Author: your name
- * @Date: 2020-09-30 14:33:38
- * @LastEditTime: 2020-09-30 14:37:46
+ * @Date: 2020-10-14 16:04:30
+ * @LastEditTime: 2020-10-30 16:03:33
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
- * @FilePath: \leetcode\003.cpp
+ * @FilePath: \leetcode\0766\0766.hpp
  */
+
 #include <cmath>
 #include <string>
 #include <iostream>
@@ -16,21 +17,21 @@ using namespace std;
 
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-		int temp = 0;
-		int result = 0;
-		int size = s.size();
-		unordered_set<char> sub;
-		int next = -1;
-		for (int i = 0; i < size; ++i) {
-			if (0 != i) {
-				sub.erase(s[i - 1]);
+    bool isToeplitzMatrix(vector<vector<int>>& matrix) {
+		auto o_size = matrix.size();
+        auto i_size = matrix[0].size();
+		for(int i = 0 ; i < o_size; ++i) {
+			auto anchor = matrix[i][0];
+			for(int j = i + 1, k = 1; j < o_size && k < i_size; ++j, ++k) {
+				if(matrix[j][k] != anchor) return false;
 			}
-			while ((next < size - 1) && (!sub.count(s[next + 1]))) {
-				sub.insert(s[(next++) + 1]);
-			}
-			result = max(next - i + 1, result);
 		}
-		return result;
+		for(int i = 0 ; i < i_size; ++i) {
+			auto anchor = matrix[0][i];
+			for(int j = 1, k = i + 1; j < o_size && k < i_size; ++j, ++k) {
+				if(matrix[j][k] != anchor) return false;
+			}
+		}
+		return true;
     }
 };
