92ebb67c9db82e9968286cd8fc36cb17007abc49
wong <degawong@hotmail.com>
2020-10-30 13:37:41 +0800

    [0234]

--- 0234/0234.hpp
+++ 0234/0234.hpp
@@ -1,12 +1,13 @@
 /*
  * @Author: your name
  * @Date: 2020-09-30 14:33:38
- * @LastEditTime: 2020-09-30 14:37:46
+ * @LastEditTime: 2020-10-30 13:36:31
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \leetcode\003.cpp
  */
 #include <cmath>
+#include <deque>
 #include <string>
 #include <iostream>
 #include <algorithm>
@@ -14,23 +15,27 @@
 
 using namespace std;
 
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(nullptr) {}
+};
+ 
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-		int temp = 0;
-		int result = 0;
-		int size = s.size();
-		unordered_set<char> sub;
-		int next = -1;
-		for (int i = 0; i < size; ++i) {
-			if (0 != i) {
-				sub.erase(s[i - 1]);
-			}
-			while ((next < size - 1) && (!sub.count(s[next + 1]))) {
-				sub.insert(s[(next++) + 1]);
-			}
-			result = max(next - i + 1, result);
+    bool isPalindrome(ListNode* head) {
+        deque<ListNode*> dq;
+		while(head != nullptr) {
+			dq.push_back(head);
+			head = head->next;
 		}
-		return result;
+		while(!dq.empty()) {
+			auto l = dq.front(); dq.pop_front();
+			if(!dq.empty()) {
+				auto r = dq.back(); dq.pop_back();			
+				if(l->val != r->val) return false;
+			}
+		} 
+		return true;      
     }
 };
