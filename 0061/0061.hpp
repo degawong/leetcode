c6df420d61084fe943b59a71dcc93d90eafc6d48
wong <degawong@hotmail.com>
2020-10-30 10:36:16 +0800

    [0061]

--- 0061/0061.hpp
+++ 0061/0061.hpp
@@ -1,11 +1,12 @@
 /*
  * @Author: your name
- * @Date: 2020-09-30 14:33:38
- * @LastEditTime: 2020-09-30 14:37:46
+ * @Date: 2020-10-14 16:04:26
+ * @LastEditTime: 2020-10-30 10:18:54
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
- * @FilePath: \leetcode\003.cpp
+ * @FilePath: \leetcode\0061\0061.hpp
  */
+
 #include <cmath>
 #include <string>
 #include <iostream>
@@ -14,23 +15,31 @@
 
 using namespace std;
 
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(nullptr) {}
+};
+ 
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-		int temp = 0;
-		int result = 0;
-		int size = s.size();
-		unordered_set<char> sub;
-		int next = -1;
-		for (int i = 0; i < size; ++i) {
-			if (0 != i) {
-				sub.erase(s[i - 1]);
-			}
-			while ((next < size - 1) && (!sub.count(s[next + 1]))) {
-				sub.insert(s[(next++) + 1]);
-			}
-			result = max(next - i + 1, result);
+    ListNode* rotateRight(ListNode* head, int k) {
+		auto anchor = head;
+		if(head == nullptr) return nullptr;
+		while(k-- > 0) {
+			if(anchor->next == nullptr) anchor = head;
+			else anchor = anchor->next;
 		}
-		return result;
+		auto ret = anchor;
+		if(anchor == head) return head;
+		while(anchor->next != nullptr) {
+			anchor = anchor->next;
+		}
+		anchor->next = head;
+		while(head->next != ret) {
+			head = head->next;
+		}
+		head->next = nullptr;
+		return ret;
     }
 };
