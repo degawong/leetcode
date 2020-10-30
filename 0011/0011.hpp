9443ef0b6f0a476896c930327326a1b91979742b
wong <degawong@hotmail.com>
2020-10-30 16:56:57 +0800

    [0011]

--- 0011/0011.hpp
+++ 0011/0011.hpp
@@ -1,11 +1,12 @@
 /*
  * @Author: your name
- * @Date: 2020-09-30 14:33:38
- * @LastEditTime: 2020-09-30 14:37:46
+ * @Date: 2020-10-14 16:04:25
+ * @LastEditTime: 2020-10-30 16:53:18
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
- * @FilePath: \leetcode\003.cpp
+ * @FilePath: \leetcode\0011\0011.hpp
  */
+
 #include <cmath>
 #include <string>
 #include <iostream>
@@ -16,21 +17,15 @@ using namespace std;
 
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-		int temp = 0;
-		int result = 0;
-		int size = s.size();
-		unordered_set<char> sub;
-		int next = -1;
-		for (int i = 0; i < size; ++i) {
-			if (0 != i) {
-				sub.erase(s[i - 1]);
-			}
-			while ((next < size - 1) && (!sub.count(s[next + 1]))) {
-				sub.insert(s[(next++) + 1]);
-			}
-			result = max(next - i + 1, result);
+    int maxArea(vector<int>& height) {
+		auto left = 0;
+		auto ret = INT_MIN;
+		auto right = height.size() - 1;
+		while(left < right) {
+			ret = max(ret, (int)(right - left) * min(height[left], height[right]));
+			if(height[left] < height[right]) ++left;
+			else --right;
 		}
-		return result;
+		return ret;
     }
 };
