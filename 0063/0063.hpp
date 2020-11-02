5424d524cadb933fe77d8de25fd13ba2fb1e81cc
wong <degawong@hotmail.com>
2020-11-02 17:29:10 +0800

    [0063]

--- 0063/0063.hpp
+++ 0063/0063.hpp
@@ -1,11 +1,12 @@
 /*
  * @Author: your name
- * @Date: 2020-09-30 14:33:38
- * @LastEditTime: 2020-09-30 14:37:46
+ * @Date: 2020-10-20 16:04:47
+ * @LastEditTime: 2020-11-02 17:27:18
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
- * @FilePath: \leetcode\003.cpp
+ * @FilePath: \leetcode\0063\0063.hpp
  */
+
 #include <cmath>
 #include <string>
 #include <iostream>
@@ -16,21 +17,20 @@ using namespace std;
 
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-		int temp = 0;
-		int result = 0;
-		int size = s.size();
-		unordered_set<char> sub;
-		int next = -1;
-		for (int i = 0; i < size; ++i) {
-			if (0 != i) {
-				sub.erase(s[i - 1]);
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+		if(obstacleGrid.size() == 0 || obstacleGrid[0].size() == 0) return 0;
+        vector<vector<int>> dp(obstacleGrid.size() + 1, vector<int>(obstacleGrid[0].size() + 1, 0));
+		for(int i = 1; i < obstacleGrid.size() + 1; ++i) {
+			for(int j = 1; j < obstacleGrid[0].size() + 1; ++j) {
+				if(i == 1 && j == 1) {
+					if(obstacleGrid[0][0] == 1) return 0;
+					else dp[1][1] = 1;
+				} else {
+					if (obstacleGrid[i-1][j-1] == 1) dp[i][j] = 0;
+                    else dp[i][j] = dp[i-1][j] + dp[i][j-1];
+				}
 			}
-			while ((next < size - 1) && (!sub.count(s[next + 1]))) {
-				sub.insert(s[(next++) + 1]);
-			}
-			result = max(next - i + 1, result);
 		}
-		return result;
+		return dp[obstacleGrid.size()][obstacleGrid[0].size()]; 
     }
 };
