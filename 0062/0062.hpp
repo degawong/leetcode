9adb21e0d8bf43990ecb1ccccfcaf99c02e06f8f
wong <degawong@hotmail.com>
2020-11-02 17:40:51 +0800

    [0062]

--- 0062/0062.hpp
+++ 0062/0062.hpp
@@ -1,11 +1,12 @@
 /*
  * @Author: your name
  * @Date: 2020-10-14 16:04:26
- * @LastEditTime: 2020-10-20 14:35:42
+ * @LastEditTime: 2020-11-02 17:40:13
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \leetcode\0062\0062.hpp
  */
+
 
 #include <cmath>
 #include <string>
@@ -17,11 +18,13 @@ using namespace std;
 
 class Solution {
 public:
-	int uniquePaths(int m, int n) {
-        vector<vector<int> > dp(m, vector<int>(n, 1));
-        for(int i = 1; i < m; ++i)
-            for(int j = 1; j < n; ++j)
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];       
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n, 1));
+        for(int i = 1; i < m; ++i) {
+            for(int j = 1; j < n; ++j) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
         return dp[m-1][n-1];
-	}
+    }
 };
