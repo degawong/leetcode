552eabf25bb7cd985697283afe07b46982882921
wong <degawong@hotmail.com>
2020-11-05 15:04:01 +0800

    [0138]

--- 0138/0138.hpp
+++ 0138/0138.hpp
@@ -1,36 +1,47 @@
 /*
  * @Author: your name
- * @Date: 2020-09-30 14:33:38
- * @LastEditTime: 2020-09-30 14:37:46
+ * @Date: 2020-10-14 16:04:26
+ * @LastEditTime: 2020-11-05 15:01:01
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
- * @FilePath: \leetcode\003.cpp
+ * @FilePath: \leetcode\0138\0138.hpp
  */
+
 #include <cmath>
 #include <string>
 #include <iostream>
 #include <algorithm>
 #include <unordered_set>
+#include <unordered_map>
 
 using namespace std;
 
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = nullptr;
+        random = nullptr;
+    }
+};
+
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-		int temp = 0;
-		int result = 0;
-		int size = s.size();
-		unordered_set<char> sub;
-		int next = -1;
-		for (int i = 0; i < size; ++i) {
-			if (0 != i) {
-				sub.erase(s[i - 1]);
-			}
-			while ((next < size - 1) && (!sub.count(s[next + 1]))) {
-				sub.insert(s[(next++) + 1]);
-			}
-			result = max(next - i + 1, result);
+    Node* copyRandomList(Node* head) {		
+        if(head == nullptr) return nullptr;
+		if(um.find(head) != um.end()) {
+			return um[head];
 		}
-		return result;
+		auto node = new Node(head->val);
+		um[head] = node;
+		node->next = copyRandomList(head->next);
+		node->random = copyRandomList(head->random);
+		return node;
     }
+private:
+	unordered_map<Node*, Node*> um;
 };
