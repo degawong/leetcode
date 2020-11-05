251cb23d12a0de70cce30944fe504cf726771cee
wong <degawong@hotmail.com>
2020-11-05 17:14:40 +0800

    [0114]

--- 0114/0114.hpp
+++ 0114/0114.hpp
@@ -1,12 +1,15 @@
 /*
  * @Author: your name
- * @Date: 2020-09-30 14:33:38
- * @LastEditTime: 2020-09-30 14:37:46
+ * @Date: 2020-10-14 16:04:26
+ * @LastEditTime: 2020-11-05 17:11:41
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
- * @FilePath: \leetcode\003.cpp
+ * @FilePath: \leetcode\0114\0114.hpp
  */
+
 #include <cmath>
+#include <deque>
+#include <stack>
 #include <string>
 #include <iostream>
 #include <algorithm>
@@ -14,23 +17,107 @@
 
 using namespace std;
 
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-		int temp = 0;
-		int result = 0;
-		int size = s.size();
-		unordered_set<char> sub;
-		int next = -1;
-		for (int i = 0; i < size; ++i) {
-			if (0 != i) {
-				sub.erase(s[i - 1]);
+    void flatten(TreeNode* root) {
+		deque<TreeNode*> dq;
+		if(root == nullptr) return;
+		auto node = root;
+		dq.push_back(node);
+		auto recorder = (TreeNode*)nullptr;
+		while (!dq.empty()) {
+			auto node = dq.back(); 
+			dq.pop_back();
+			if(recorder != nullptr) {
+				recorder->left = nullptr;
+				recorder->right = node;
 			}
-			while ((next < size - 1) && (!sub.count(s[next + 1]))) {
-				sub.insert(s[(next++) + 1]);
-			}
-			result = max(next - i + 1, result);
+			if(node->right != nullptr) dq.push_back(node->right);
+			if(node->left != nullptr) dq.push_back(node->left);
+			recorder = node;
 		}
-		return result;
-    }
+	}
 };
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+// class Solution {
+// public:
+//     void flatten(TreeNode* root) {
+//         root = build(root);
+//     }
+
+//     TreeNode*build(TreeNode*root)
+//     {
+        
+//         if(!root||root->right==NULL&&root->left==NULL)//叶子节点，空节点直接返回
+//             return root;
+//         //先序遍历建立链表
+//         root->left = build(root->left);
+//         root->right = build(root->right);
+//         //找到左子树的最右端;
+//         TreeNode*p = root->left;
+//         if(p)
+//         {
+//             while(p->right!=NULL)
+//                 p=p->right;
+//             p->right = root->right;
+//             root->right = root->left;
+//         }
+//         root->left = NULL;
+//         return root;
+//     }
+// };
+
+// 作者：unicornss
+// 链接：https://leetcode-cn.com/problems/flatten-binary-tree-to-linked-list/solution/lao-nian-ren-di-gui-si-lu-by-unicornss/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+// class Solution {
+// public:
+//     void flatten(TreeNode* root) {
+//         if (root == nullptr) {
+//             return;
+//         }
+//         auto stk = stack<TreeNode*>();
+//         stk.push(root);
+//         TreeNode *prev = nullptr;
+//         while (!stk.empty()) {
+//             TreeNode *curr = stk.top(); stk.pop();
+//             if (prev != nullptr) {
+//                 prev->left = nullptr;
+//                 prev->right = curr;
+//             }
+//             TreeNode *left = curr->left, *right = curr->right;
+//             if (right != nullptr) {
+//                 stk.push(right);
+//             }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
+//             if (left != nullptr) {
+//                 stk.push(left);
+//             }
+//             prev = curr;
+//         }
+//     }
+// };
+
+// 作者：LeetCode-Solution
+// 链接：https://leetcode-cn.com/problems/flatten-binary-tree-to-linked-list/solution/er-cha-shu-zhan-kai-wei-lian-biao-by-leetcode-solu/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
