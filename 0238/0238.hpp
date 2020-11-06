7b82ee80d938fd0ecb180eee681a231b3b69b173
wong <degawong@hotmail.com>
2020-11-06 15:47:10 +0800

    [0238]

--- 0238/0238.hpp
+++ 0238/0238.hpp
@@ -1,11 +1,12 @@
 /*
  * @Author: your name
- * @Date: 2020-09-30 14:33:38
- * @LastEditTime: 2020-09-30 14:37:46
+ * @Date: 2020-10-14 16:04:27
+ * @LastEditTime: 2020-11-06 15:45:47
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
- * @FilePath: \leetcode\003.cpp
+ * @FilePath: \leetcode\0238\0238.hpp
  */
+
 #include <cmath>
 #include <string>
 #include <iostream>
@@ -16,21 +17,18 @@ using namespace std;
 
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-		int temp = 0;
-		int result = 0;
-		int size = s.size();
-		unordered_set<char> sub;
-		int next = -1;
-		for (int i = 0; i < size; ++i) {
-			if (0 != i) {
-				sub.erase(s[i - 1]);
-			}
-			while ((next < size - 1) && (!sub.count(s[next + 1]))) {
-				sub.insert(s[(next++) + 1]);
-			}
-			result = max(next - i + 1, result);
+    vector<int> productExceptSelf(vector<int>& nums) {
+		int size = nums.size();
+		vector<int> ret(size, 0);
+		vector<int> lm(size, nums[0]);
+		vector<int> rm(size, nums[size - 1]);
+		for(int i = 1; i < size; ++i) {
+			lm[i] = lm[i - 1] * nums[i];
+			rm[size - i - 1] = rm[size - i] * nums[size - i - 1];
 		}
-		return result;
+		for(int i = 1; i < size - 1; ++i) ret[i] = lm[i - 1] * rm[i + 1];
+		ret[0] = rm[1];
+		ret[size - 1] = lm[size - 1 - 1];
+		return ret;
     }
 };
