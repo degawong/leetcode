d9d7a2d4b91e86021bb1da5086f21c832e161134
wong <degawong@hotmail.com>
2020-11-06 14:46:22 +0800

    [0208]

--- 0208/0208.hpp
+++ 0208/0208.hpp
@@ -1,11 +1,14 @@
 /*
  * @Author: your name
- * @Date: 2020-09-30 14:33:38
- * @LastEditTime: 2020-09-30 14:37:46
+ * @Date: 2020-10-14 16:04:27
+ * @LastEditTime: 2020-11-06 14:43:13
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
- * @FilePath: \leetcode\003.cpp
+ * @FilePath: \leetcode\0208\0208.hpp
  */
+
+#include <map>
+#include <set>
 #include <cmath>
 #include <string>
 #include <iostream>
@@ -14,23 +17,32 @@
 
 using namespace std;
 
-class Solution {
+class Trie {
 public:
-    int lengthOfLongestSubstring(string s) {
-		int temp = 0;
-		int result = 0;
-		int size = s.size();
-		unordered_set<char> sub;
-		int next = -1;
-		for (int i = 0; i < size; ++i) {
-			if (0 != i) {
-				sub.erase(s[i - 1]);
-			}
-			while ((next < size - 1) && (!sub.count(s[next + 1]))) {
-				sub.insert(s[(next++) + 1]);
-			}
-			result = max(next - i + 1, result);
-		}
-		return result;
+    /** Initialize your data structure here. */
+    Trie() {
+
     }
+    
+    /** Inserts a word into the trie. */
+    void insert(string word) {
+		s.insert(word);
+    }
+    
+    /** Returns if the word is in the trie. */
+    bool search(string word) {
+		return s.count(word) != 0;
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    bool startsWith(string prefix) {
+		auto ret = false;
+		for(auto& r : s) {
+			if(r.size() < prefix.size()) continue;
+			if(prefix == r.substr(0, prefix.size())) return true;
+		}
+		return ret;
+    }
+private:
+	set<string> s;
 };
