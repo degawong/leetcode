c8d5541dd4266e3ffd336a788f3a033c65441227
wong <degawong@hotmail.com>
2020-11-09 16:56:03 +0800

    [0669]

--- 0669/0669.hpp
+++ 0669/0669.hpp
@@ -1,11 +1,12 @@
 /*
  * @Author: your name
- * @Date: 2020-09-30 14:33:38
- * @LastEditTime: 2020-09-30 14:37:46
+ * @Date: 2020-10-14 16:04:30
+ * @LastEditTime: 2020-11-09 16:53:28
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
- * @FilePath: \leetcode\003.cpp
+ * @FilePath: \leetcode\0669\0669.hpp
  */
+
 #include <cmath>
 #include <string>
 #include <iostream>
@@ -14,23 +15,37 @@
 
 using namespace std;
 
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+ 
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-		int temp = 0;
-		int result = 0;
-		int size = s.size();
-		unordered_set<char> sub;
-		int next = -1;
-		for (int i = 0; i < size; ++i) {
-			if (0 != i) {
-				sub.erase(s[i - 1]);
-			}
-			while ((next < size - 1) && (!sub.count(s[next + 1]))) {
-				sub.insert(s[(next++) + 1]);
-			}
-			result = max(next - i + 1, result);
-		}
-		return result;
+    TreeNode* trimBST(TreeNode* root, int low, int high) {
+		return dfs(root, low, high);
     }
+private:
+	bool to_trim(int val, int low, int high) {
+		return (val < low || val > high);
+	}
+	// root  shall not be nullptr
+	TreeNode* dfs(TreeNode* root, int low, int high) {
+		auto ret = root;
+		if(root == nullptr) return ret;
+		if(root->val > high) {
+			ret = dfs(root->left, low, high);
+		} else if(root->val < low) {
+			ret = dfs(root->right, low, high);
+		} else {
+			root->left = dfs(root->left, low, high);
+			root->right = dfs(root->right, low, high);
+			ret = root;
+		}
+		return ret;
+	}
 };
