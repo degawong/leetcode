b2713192cfa487c9aa095e3e68841c0dc1a34770
wong <degawong@hotmail.com>
2020-11-09 17:05:09 +0800

    [0700]

--- 0700/0700.hpp
+++ 0700/0700.hpp
@@ -1,11 +1,12 @@
 /*
  * @Author: your name
- * @Date: 2020-09-30 14:33:38
- * @LastEditTime: 2020-09-30 14:37:46
+ * @Date: 2020-10-14 16:04:30
+ * @LastEditTime: 2020-11-09 17:04:14
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
- * @FilePath: \leetcode\003.cpp
+ * @FilePath: \leetcode\0700\0700.hpp
  */
+
 #include <cmath>
 #include <string>
 #include <iostream>
@@ -14,23 +15,20 @@
 
 using namespace std;
 
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+ 
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-		int temp = 0;
-		int result = 0;
-		int size = s.size();
-		unordered_set<char> sub;
-		int next = -1;
-		for (int i = 0; i < size; ++i) {
-			if (0 != i) {
-				sub.erase(s[i - 1]);
-			}
-			while ((next < size - 1) && (!sub.count(s[next + 1]))) {
-				sub.insert(s[(next++) + 1]);
-			}
-			result = max(next - i + 1, result);
-		}
-		return result;
+    TreeNode* searchBST(TreeNode* root, int val) {
+		auto ret = root;
+		if(root == nullptr || root->val == val) return ret;
+		if(root->val > val) ret = searchBST(root->left, val);
+		else ret = searchBST(root->right, val);
+		return ret;
     }
 };
