4eac75fb91997472c41005731eb9de00189750a3
wong <degawong@hotmail.com>
2020-11-10 17:39:38 +0800

    [0589]

--- 0589/0589.hpp
+++ 0589/0589.hpp
@@ -1,11 +1,12 @@
 /*
  * @Author: your name
- * @Date: 2020-09-30 14:33:38
- * @LastEditTime: 2020-09-30 14:37:46
+ * @Date: 2020-10-14 16:04:29
+ * @LastEditTime: 2020-11-10 17:39:04
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
- * @FilePath: \leetcode\003.cpp
+ * @FilePath: \leetcode\0589\0589.hpp
  */
+
 #include <cmath>
 #include <string>
 #include <iostream>
@@ -14,23 +15,36 @@
 
 using namespace std;
 
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-		int temp = 0;
-		int result = 0;
-		int size = s.size();
-		unordered_set<char> sub;
-		int next = -1;
-		for (int i = 0; i < size; ++i) {
-			if (0 != i) {
-				sub.erase(s[i - 1]);
-			}
-			while ((next < size - 1) && (!sub.count(s[next + 1]))) {
-				sub.insert(s[(next++) + 1]);
-			}
-			result = max(next - i + 1, result);
-		}
-		return result;
+    vector<int> preorder(Node* root) {
+        dfs(root);
+		return ret;
     }
+private:
+	vector<int> ret;
+	void dfs(Node* root) {
+		if(root == nullptr) return;
+		ret.push_back(root->val);
+		for(auto n : root->children) {
+			dfs(n);
+		}
+	}
 };
