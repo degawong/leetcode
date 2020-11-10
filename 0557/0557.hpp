57370276f0eaa1db76ec4f3d0a30bea355b7c32a
wong <degawong@hotmail.com>
2020-11-10 17:27:45 +0800

    [0557]

--- 0557/0557.hpp
+++ 0557/0557.hpp
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-09-30 14:33:38
- * @LastEditTime: 2020-09-30 14:37:46
+ * @LastEditTime: 2020-11-10 17:27:35
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \leetcode\003.cpp
@@ -16,21 +16,20 @@ using namespace std;
 
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-		int temp = 0;
-		int result = 0;
-		int size = s.size();
-		unordered_set<char> sub;
-		int next = -1;
-		for (int i = 0; i < size; ++i) {
-			if (0 != i) {
-				sub.erase(s[i - 1]);
-			}
-			while ((next < size - 1) && (!sub.count(s[next + 1]))) {
-				sub.insert(s[(next++) + 1]);
-			}
-			result = max(next - i + 1, result);
-		}
-		return result;
+    string reverseWords(string s) {
+        bool run = true;
+        if(s.size() == 0) return s;
+        int l = 0;
+        int r = 0;
+        while(run) {
+            while(s[r] != ' ' && r < s.size()) ++r;
+            int temp = r;
+            if(r == s.size()) run = false;
+            while (l < (r - 1)) {
+                swap(s[l++], s[r-- - 1]);
+            }
+            l = r = temp + 1;
+        }
+        return s;
     }
 };
