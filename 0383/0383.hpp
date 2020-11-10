a62c01541b31b81a7f1000acfbba354f3515962e
wong <degawong@hotmail.com>
2020-11-10 16:07:49 +0800

    [0383]

--- 0383/0383.hpp
+++ 0383/0383.hpp
@@ -1,11 +1,12 @@
 /*
  * @Author: your name
- * @Date: 2020-09-30 14:33:38
- * @LastEditTime: 2020-09-30 14:37:46
+ * @Date: 2020-10-14 16:04:28
+ * @LastEditTime: 2020-11-10 16:07:39
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
- * @FilePath: \leetcode\003.cpp
+ * @FilePath: \leetcode\0383\0383.hpp
  */
+
 #include <cmath>
 #include <string>
 #include <iostream>
@@ -16,21 +17,12 @@ using namespace std;
 
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-		int temp = 0;
-		int result = 0;
-		int size = s.size();
-		unordered_set<char> sub;
-		int next = -1;
-		for (int i = 0; i < size; ++i) {
-			if (0 != i) {
-				sub.erase(s[i - 1]);
-			}
-			while ((next < size - 1) && (!sub.count(s[next + 1]))) {
-				sub.insert(s[(next++) + 1]);
-			}
-			result = max(next - i + 1, result);
+    bool canConstruct(string ransomNote, string magazine) {
+        int hash[26] = {0};
+        for (char c : magazine) hash[c - 'a'] += 1;
+        for (char c : ransomNote) {
+            if ((hash[c - 'a'] -= 1) < 0) return false;
 		}
-		return result;
+        return true;
     }
 };
