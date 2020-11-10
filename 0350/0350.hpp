13a27c8fc2db7c3817d2cc56c9c3fd6255e496ce
wong <degawong@hotmail.com>
2020-11-10 15:20:20 +0800

    [0350]

--- 0350/0350.hpp
+++ 0350/0350.hpp
@@ -1,11 +1,13 @@
 /*
  * @Author: your name
- * @Date: 2020-09-30 14:33:38
- * @LastEditTime: 2020-09-30 14:37:46
+ * @Date: 2020-10-14 16:04:28
+ * @LastEditTime: 2020-11-10 15:19:18
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
- * @FilePath: \leetcode\003.cpp
+ * @FilePath: \leetcode\0350\0350.hpp
  */
+
+#include <map>
 #include <cmath>
 #include <string>
 #include <iostream>
@@ -16,21 +18,19 @@ using namespace std;
 
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-		int temp = 0;
-		int result = 0;
-		int size = s.size();
-		unordered_set<char> sub;
-		int next = -1;
-		for (int i = 0; i < size; ++i) {
-			if (0 != i) {
-				sub.erase(s[i - 1]);
-			}
-			while ((next < size - 1) && (!sub.count(s[next + 1]))) {
-				sub.insert(s[(next++) + 1]);
-			}
-			result = max(next - i + 1, result);
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+		vector<int> ret;        
+		map<int, int> cm;
+        map<int, int> fm;
+		for(auto n : nums1) {
+            ++cm[n];
+        }
+		for(auto n : nums2) {
+			if(cm.count(n) != 0) ++fm[n];
 		}
-		return result;
+        for(auto& n : fm) {
+			for(int i = 0; i < min(cm[n.first], fm[n.first]); ++i) ret.push_back(n.first);			
+		}
+		return ret;
     }
 };
