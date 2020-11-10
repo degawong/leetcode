3421ab3a1a945b7d68754f7c6800d79bf8bcddbd
wong <degawong@hotmail.com>
2020-11-10 17:32:31 +0800

    [0559]

--- 0559/0559.hpp
+++ 0559/0559.hpp
@@ -1,11 +1,12 @@
 /*
  * @Author: your name
- * @Date: 2020-09-30 14:33:38
- * @LastEditTime: 2020-09-30 14:37:46
+ * @Date: 2020-10-14 16:04:29
+ * @LastEditTime: 2020-11-10 17:32:18
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
- * @FilePath: \leetcode\003.cpp
+ * @FilePath: \leetcode\0559\0559.hpp
  */
+
 #include <cmath>
 #include <string>
 #include <iostream>
@@ -13,24 +14,31 @@
 #include <unordered_set>
 
 using namespace std;
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
 
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-		int temp = 0;
-		int result = 0;
-		int size = s.size();
-		unordered_set<char> sub;
-		int next = -1;
-		for (int i = 0; i < size; ++i) {
-			if (0 != i) {
-				sub.erase(s[i - 1]);
-			}
-			while ((next < size - 1) && (!sub.count(s[next + 1]))) {
-				sub.insert(s[(next++) + 1]);
-			}
-			result = max(next - i + 1, result);
+    int maxDepth(Node* root) {
+        int ret = 0;
+		if(root == nullptr) return 0;
+		for(auto n : root->children){
+			ret = max(ret, maxDepth(n));
 		}
-		return result;
+		return ret + 1;
     }
 };
