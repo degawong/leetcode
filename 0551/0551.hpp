f520eeb184434d7f59b70c531db72b650f993052
wong <degawong@hotmail.com>
2020-11-10 17:14:15 +0800

    [0551]

--- 0551/0551.hpp
+++ 0551/0551.hpp
@@ -1,11 +1,12 @@
 /*
  * @Author: your name
- * @Date: 2020-09-30 14:33:38
- * @LastEditTime: 2020-09-30 14:37:46
+ * @Date: 2020-10-14 16:04:29
+ * @LastEditTime: 2020-11-10 17:12:48
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
- * @FilePath: \leetcode\003.cpp
+ * @FilePath: \leetcode\0551\0551.hpp
  */
+
 #include <cmath>
 #include <string>
 #include <iostream>
@@ -13,24 +14,17 @@
 #include <unordered_set>
 
 using namespace std;
-
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-		int temp = 0;
-		int result = 0;
-		int size = s.size();
-		unordered_set<char> sub;
-		int next = -1;
-		for (int i = 0; i < size; ++i) {
-			if (0 != i) {
-				sub.erase(s[i - 1]);
-			}
-			while ((next < size - 1) && (!sub.count(s[next + 1]))) {
-				sub.insert(s[(next++) + 1]);
-			}
-			result = max(next - i + 1, result);
+    bool checkRecord(string s) {
+		int l = 0;
+		int ab = { 0 };
+		for(int i = 0; i < s.size(); ++i) {
+			if(s[i] == 'L') ++l;
+			else l = 0;
+			if(s[i] == 'A') ++ab;
+			if(l > 2 || ab > 1) return false;
 		}
-		return result;
+        return true;
     }
 };
