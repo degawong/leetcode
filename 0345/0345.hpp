e2376f8704d8b70c3a3794896cf51213e6a4ad53
wong <degawong@hotmail.com>
2020-11-10 14:55:17 +0800

    [0345]

--- 0345/0345.hpp
+++ 0345/0345.hpp
@@ -1,11 +1,12 @@
 /*
  * @Author: your name
- * @Date: 2020-09-30 14:33:38
- * @LastEditTime: 2020-09-30 14:37:46
+ * @Date: 2020-10-14 16:04:28
+ * @LastEditTime: 2020-11-10 14:51:15
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
- * @FilePath: \leetcode\003.cpp
+ * @FilePath: \leetcode\0345\0345.hpp
  */
+
 #include <cmath>
 #include <string>
 #include <iostream>
@@ -16,21 +17,19 @@ using namespace std;
 
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-		int temp = 0;
-		int result = 0;
-		int size = s.size();
-		unordered_set<char> sub;
-		int next = -1;
-		for (int i = 0; i < size; ++i) {
-			if (0 != i) {
-				sub.erase(s[i - 1]);
-			}
-			while ((next < size - 1) && (!sub.count(s[next + 1]))) {
-				sub.insert(s[(next++) + 1]);
-			}
-			result = max(next - i + 1, result);
+    string reverseVowels(string s) {
+        if(s.size() == 0) return s;
+		int l = 0;
+		int r = s.size() - 1;
+		while(l < r) {
+			if(!is_vowel(s[l])) {++l;continue;}
+			if(!is_vowel(s[r])) {--r;continue;}
+			swap(s[l++], s[r--]);
 		}
-		return result;
+		return s;
     }
+private:
+	bool is_vowel(char c) {
+		return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
+	}
 };
