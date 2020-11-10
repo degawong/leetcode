a4f06208a49e6105085cffb0ba657e0b9e06f058
wong <degawong@hotmail.com>
2020-11-10 14:41:46 +0800

    [0344]

--- 0344/0344.hpp
+++ 0344/0344.hpp
@@ -1,11 +1,4 @@
-/*
- * @Author: your name
- * @Date: 2020-09-30 14:33:38
- * @LastEditTime: 2020-09-30 14:37:46
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: \leetcode\003.cpp
- */
+
 #include <cmath>
 #include <string>
 #include <iostream>
@@ -16,21 +9,12 @@ using namespace std;
 
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-		int temp = 0;
-		int result = 0;
-		int size = s.size();
-		unordered_set<char> sub;
-		int next = -1;
-		for (int i = 0; i < size; ++i) {
-			if (0 != i) {
-				sub.erase(s[i - 1]);
-			}
-			while ((next < size - 1) && (!sub.count(s[next + 1]))) {
-				sub.insert(s[(next++) + 1]);
-			}
-			result = max(next - i + 1, result);
+    void reverseString(vector<char>& s) {
+        if(s.size() == 0) return;
+		int l = 0;
+		int r = s.size() - 1;
+		while(l < r) {
+			swap(s[l], s[r]);
 		}
-		return result;
     }
 };
