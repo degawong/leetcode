63dce19db293d76d025f785cbee16756b9744662
wong <degawong@hotmail.com>
2020-11-11 10:59:42 +0800

    [0094]

--- 0094/0094.hpp
+++ 0094/0094.hpp
@@ -1,13 +1,14 @@
 /*
  * @Author: your name
  * @Date: 2020-10-14 16:04:26
- * @LastEditTime: 2020-10-19 11:30:10
+ * @LastEditTime: 2020-11-11 10:59:22
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \leetcode\0094\0094.hpp
  */
 
 #include <cmath>
+#include <stack>
 #include <string>
 #include <iostream>
 #include <algorithm>
@@ -29,10 +30,16 @@ struct TreeNode {
 // public:
 //     vector<int> inorderTraversal(TreeNode* root) {
 //         vector<int> ret;
-// 		TreeNode * l = root;
-// 		while(l)
+// 		inorder(root, ret);
 // 		return ret;
 //     }
+// private:
+// 	void inorder(TreeNode* root, vector<int>& v) {
+// 		if(root == nullptr) return;
+// 		inorder(root->left, v);
+// 		v.push_back(root->val);
+// 		inorder(root->right, v);
+// 	}
 // };
 
 class Solution {
@@ -40,14 +47,24 @@ class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> ret;
-		inorder(root, ret);
+		stack<TreeNode*> stk;
+		stk.push(root);
+        if(root == nullptr) return ret;
+		while(!stk.empty()) {
+			auto node = stk.top();
+			if(node == nullptr) {
+				stk.pop();
+				auto n = stk.top();
+				stk.pop();
+				ret.push_back(n->val);
+			} else {
+				stk.pop();
+				if(node->right != nullptr) stk.push(node->right);
+				stk.push(node);
+				stk.push(nullptr);
+				if(node->left != nullptr) stk.push(node->left);
+			}
+		}
 		return ret;
     }
-private:
-	void inorder(TreeNode* root, vector<int>& v) {
-		if(root == nullptr) return;
-		inorder(root->left, v);
-		v.push_back(root->val);
-		inorder(root->right, v);
-	}
 };
