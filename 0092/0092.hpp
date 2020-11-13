2d089ee8f9fef7c6c0bf9054d2bcd0f9293ae893
wong <degawong@hotmail.com>
2020-11-13 15:35:54 +0800

    [0092]

--- 0092/0092.hpp
+++ 0092/0092.hpp
@@ -1,11 +1,12 @@
 /*
  * @Author: your name
- * @Date: 2020-09-30 14:33:38
- * @LastEditTime: 2020-09-30 14:37:46
+ * @Date: 2020-10-19 15:12:23
+ * @LastEditTime: 2020-11-13 15:30:31
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
- * @FilePath: \leetcode\003.cpp
+ * @FilePath: \leetcode\0092\0092.hpp
  */
+
 #include <cmath>
 #include <string>
 #include <iostream>
@@ -14,23 +15,41 @@
 
 using namespace std;
 
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(nullptr) {}
+};
+ 
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-		int temp = 0;
-		int result = 0;
-		int size = s.size();
-		unordered_set<char> sub;
-		int next = -1;
-		for (int i = 0; i < size; ++i) {
-			if (0 != i) {
-				sub.erase(s[i - 1]);
-			}
-			while ((next < size - 1) && (!sub.count(s[next + 1]))) {
-				sub.insert(s[(next++) + 1]);
-			}
-			result = max(next - i + 1, result);
+    ListNode* reverseBetween(ListNode* head, int m, int n) {
+		auto size = n - m;
+        if(head == nullptr || size == 0) return head;
+		auto dummy = new ListNode(0);
+		dummy->next = head;
+		auto anchor = dummy;
+		while(--m > 0) {
+			anchor = anchor->next;
 		}
-		return result;
+		anchor->next = reverse_by_count(anchor->next, size);
+		return dummy->next;
     }
+private:
+	ListNode* reverse_by_count(ListNode* head, int size) {
+		auto dummy = new ListNode(0);
+		dummy->next = head;
+		auto op = head;
+		auto tail = head;
+		auto recorder = (ListNode*)nullptr;
+		for(int i = 0; i < size; ++i) {
+			auto next = op->next;
+			op->next = recorder;
+			recorder = op;
+			op = next;
+		}
+		tail->next = op->next;
+		op->next = recorder;
+		return op;
+	}	
 };
--- leetcode/92.反转链表-ii.cpp
+++ leetcode/92.反转链表-ii.cpp
@@ -1,4 +1,12 @@
 /*
+ * @Author: your name
+ * @Date: 2020-10-30 10:36:42
+ * @LastEditTime: 2020-11-13 15:35:15
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \leetcode\leetcode\92.反转链表-ii.cpp
+ */
+/*
  * @lc app=leetcode.cn id=92 lang=cpp
  *
  * [92] 反转链表 II
@@ -16,8 +24,34 @@
 class Solution {
 public:
     ListNode* reverseBetween(ListNode* head, int m, int n) {
-        
+		auto size = n - m;
+        if(head == nullptr || size == 0) return head;
+		auto dummy = new ListNode(0);
+		dummy->next = head;
+		auto anchor = dummy;
+		while(--m > 0) {
+			anchor = anchor->next;
+		}
+		anchor->next = reverse_by_count(anchor->next, size);
+		return dummy->next;
     }
+private:
+	ListNode* reverse_by_count(ListNode* head, int size) {
+		auto dummy = new ListNode(0);
+		dummy->next = head;
+		auto op = head;
+		auto tail = head;
+		auto recorder = (ListNode*)nullptr;
+		for(int i = 0; i < size; ++i) {
+			auto next = op->next;
+			op->next = recorder;
+			recorder = op;
+			op = next;
+		}
+		tail->next = op->next;
+		op->next = recorder;
+		return op;
+	}	
 };
 // @lc code=end
 
